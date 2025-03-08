993768e19456f001ce5794a645d289d221add308
dp.shpakov <dp.shpakov@tensor.ru>
2025-03-09 00:52:55 +0700

    Revert "перевод автотеста на использование РОМ"
    This reverts commit 7963414f0d852213e61cb76b8254dc635bf0c48f.

--- modul_5/config.py
+++
@@ -1,7 +0,0 @@
-login_name = 'standard_user'
-password = 'secret_sauce'
-product = 4
-catalog_name = {1: 'Sauce Labs Backpack', 2: 'Sauce Labs Bike Light', 3: 'Sauce Labs Bolt T-Shirt', 4: 'Sauce Labs Fleece Jacket',
-                5: 'Sauce Labs Onesie', 6: 'Test.allTheThings() T-Shirt (Red)'}
-catalog_price = {1: '$29.99', 2: '$9.99', 3: '$15.99', 4: '$49.99', 5: '$7.99', 6: '$15.99'}
-count = '1'
